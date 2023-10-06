a5914f5fa6db0a1ecb4affcb77253e69e5dc69bd
bagashvilit <bagashvilit@allegheny.edu>
2023-10-06 00:04:46 -0400

    add catalog implementation

--- include/db/Catalog.h
+++ include/db/Catalog.h
@@ -1,6 +1,7 @@
 #ifndef DB_CATALOG_H
 #define DB_CATALOG_H
 
+#include <unordered_map>
 #include <db/TupleDesc.h>
 #include <db/DbFile.h>
 #include <db/Utility.h>
@@ -24,6 +25,8 @@ namespace db {
      */
     class Catalog {
         // TODO pa1.2: add private members
+        std::unordered_map<int, Table> tablesById;
+        std::unordered_map<std::string, int> idByName;
     public:
         // disable copy
         Catalog(const Catalog &) = delete;
